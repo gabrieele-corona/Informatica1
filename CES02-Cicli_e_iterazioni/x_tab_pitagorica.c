0c79f2f990f31bf5de1e1ee41c5f03390704cbbe
gabrieele-corona <113691350+gabrieele-corona@users.noreply.github.com>
2022-12-21 13:29:35 +0100

    Update x_tab_pitagorica.c

--- CES02-Cicli_e_iterazioni/x_tab_pitagorica.c
+++ CES02-Cicli_e_iterazioni/x_tab_pitagorica.c
@@ -15,13 +15,13 @@ int main()
 {
 	int num=1,num2=0,cont=1;    //dichiaro e avvaloro le variabili intere
 	
-	for(cont=1;cont<=10;cont++)
+	for(cont=1;cont<=10;cont++) //quando cont supera il 10 il ciclo si ferma, cont viene incrementato di uno ogni ciclo eseguito
 	{
-		for(num=1;num<=10;num++)
+		for(num=1;num<=10;num++) //for annidato che se la variabile inserita dall'utente supera 10 si ferma, il numero viene incrementato di uno ogni ciclo eseguito
 		{
 			num2+=cont;
 			
-			printf("%4d",num2);
+			printf("%4d",num2);   //stampare a schermo i numeri 
 			
 		}num2=0;
 		printf("\n\n");
